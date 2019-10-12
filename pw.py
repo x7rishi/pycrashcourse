94a64f2fd774449f4ced40830930981e77673535
x7rishi <rishi.iam@live.com>
2019-10-12 18:15:08 +0530

    few modification

--- pw.py
+++ pw.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 # pw.py -An insecure Password Locker Program
+
 PASSWORD = {'outlook' : 'rishi.iam@live.com','blog' : "sad;fljaweifjsd;lkfj ;sdflk"}
 import sys
+import pyperclip as pc
 if (len(sys.argv)<2) :
     print("Usage: python3 pw.py [account] - copy account password")
     sys.exit()
@@ -12,5 +14,8 @@ account = sys.argv[2]
 
 if account in PASSWORD :
     print("password is in the database")
+    pc.copy(PASSWORD[account])
+    print("Password for %s is copied to clipboard"%account)
+    
 else :
     print("password is not found")
